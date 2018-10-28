bd2e110c431e089a1ba36625044d17c73a86dcb9
olenalo <uleta@ukr.net>
2018-10-28 16:40:48 +0200

    Test

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -3,6 +3,7 @@ package com.alevel;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        System.out.println("Test");
     }
 }
